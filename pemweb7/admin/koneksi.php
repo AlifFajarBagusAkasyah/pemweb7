4713f54ee8d1b397451a56cfc523f0dda3de5fb5
AlifFajarBagusAkasyah <79738245+AlifFajarBagusAkasyah@users.noreply.github.com>
2021-04-27 11:06:20 +0700

    Add files via upload

---
+++ pemweb7/admin/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// Konfigurasi Database
+$host        = "localhost";
+$user        = "root";
+$password    = "12345";
+$database     = "latihan";
+
+// Perintah PHP untuk akses ke database 
+$koneksi = mysqli_connect($host, $user, $password, $database);
---
+++ pemweb7/kontak.php
@@ -0,0 +1,33 @@
+<?php
+include 'koneksi.php';
+// Menyimpan data ke dalam variabel
+$nama			= $_POST['nama'];
+$jkel			= $_POST['jkel'];
+$email			= $_POST['email'];
+$alamat			= $_POST['alamat'];
+$kota			= $_POST['kota'];
+
+
+// Query SQL untuk menambahkan data pada tabel kontak
+$query = "INSERT INTO kontak (id, nama, jkel, email, alamat, kota, pesan) VALUES (1, 'Alif fajar', 'Laki-laki', 'aliffajar@gmail.com', 'Purwoasri', 'Kediri', 'Jangan kasih kendor!')";
+mysqli_query($koneksi, $query);
+
+// Mengalihkan ke halaman tampilkontak.php
+header("location:tampilkontak.php");
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+	<style type="text/css">
+		body {
+			background-color: cyan;
+		}
+	</style>
+</body>
+
+</html>
